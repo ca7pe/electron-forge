ddecef58dd1604a8156812d2f029b0562d1107b9
Mark Lee <electronjs@lazymalevolence.com>
2018-03-10 10:32:49 -0800

    Remove shell completion support
    tabtab is rather buggy and the alternative, commander-completion, needs
    a custom completion script to be written and maintained.

--- src/electron-forge.js
+++ src/electron-forge.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import 'colors';
 import program from 'commander';
-import tabtab from 'tabtab';
 
 import './util/terminate';
 import asyncOra from './util/ora-handler';
@@ -37,39 +36,20 @@ program
   .command('publish', 'Publish the current Electron application to GitHub')
   .command('install', 'Install an Electron application from GitHub');
 
-const tab = tabtab({
-  name: 'electron-forge',
-});
-tab.on('electron-forge', (data, done) => {
-  if (data.line.split(' ').length <= 2) {
-    done(
-      null,
-      program.commands
-        .filter(cmd => cmd._name.startsWith(data.lastPartial))
-        .map(cmd => `${cmd._name}:${cmd._description}`).sort()
-    );
-  } else {
-    done(null, []);
+(async () => {
+  let goodSystem;
+  await asyncOra('Checking your system', async (ora) => {
+    goodSystem = await checkSystem(ora);
+  });
+
+  if (!goodSystem) {
+    console.error(('It looks like you are missing some dependencies you need to get Electron running.\n' +
+                  'Make sure you have git installed and Node.js version 6.0.0+').red);
+    process.exit(1);
   }
-});
-tab.start();
 
-if (process.argv[2] !== 'completion') {
-  (async () => {
-    let goodSystem;
-    await asyncOra('Checking your system', async (ora) => {
-      goodSystem = await checkSystem(ora);
-    });
+  program.parse(process.argv);
 
-    if (!goodSystem) {
-      console.error(('It looks like you are missing some dependencies you need to get Electron running.\n' +
-                    'Make sure you have git installed and Node.js version 6.0.0+').red);
-      process.exit(1);
-    }
-
-    program.parse(process.argv);
-
-    config.reset();
-    config.set('verbose', !!program.verbose);
-  })();
-}
+  config.reset();
+  config.set('verbose', !!program.verbose);
+})();
